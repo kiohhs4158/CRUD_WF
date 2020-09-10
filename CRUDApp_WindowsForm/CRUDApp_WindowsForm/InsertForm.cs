f920ff57ca5f26c371bc6ec256704420cb178565
kiohhs4158 <kiohhs4158@pasco.co.jp>
2020-09-10 18:03:19 +0900

    Add autoincrement (must move to DBManager.cs)

--- CRUDApp_WindowsForm/CRUDApp_WindowsForm/InsertForm.cs
+++ CRUDApp_WindowsForm/CRUDApp_WindowsForm/InsertForm.cs
@@ -20,6 +20,8 @@ namespace CRUDApp_WindowsForm
         private void InsertBtn2_Click(object sender, EventArgs e)
         {
             DBManager dbm = new DBManager();
+            List<object> shohin_ids = InsertForm.GetColumnList("shohin_id");
+            string s = AutoShohinID(shohin_ids);
             string tbl_name = "Shohin";
             string shohin_id = this.ShohinidmaskedTextBox.Text;
             string shohin_mei = this.ShohinmeimaskedTextBox.Text;
@@ -29,6 +31,28 @@ namespace CRUDApp_WindowsForm
             dbm.Insert(tbl_name, shohin_id, shohin_mei, shohin_bunrui, hanbai_tanka, shiire_tanka);
             this.Close();
             this.Dispose();
+        }
+
+        private static List<object> GetColumnList(string col_name)
+        {
+            DBManager dbm = new DBManager();
+            string sql = $"SELECT {col_name} FROM Shohin ORDER BY {col_name}";
+            DataSet ds = dbm.View(sql);
+            DataTable dt = ds.Tables[0];
+            DataRowCollection drc = dt.Rows;
+            List<object> col_element_list = new List<object>();
+            foreach (DataRow dr in drc)
+            {
+                col_element_list.Add(dr.ItemArray[0]);
+            }
+            return col_element_list;
+        }
+
+        private static string AutoShohinID(List<object> col_element_list)
+        {
+            int last_id = int.Parse(col_element_list.Last().ToString());
+            int insert_id = last_id + 1;
+            return String.Format("{0:D4}", insert_id);
         }
     }
 }
